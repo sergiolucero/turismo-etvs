14ee76ef950d8f09ed3ee162d519a99c9711ba85
Sergio Lucero <sergiolucero@gmail.com>
2022-07-17 07:13:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import pandas as pd
-import random
 
 st.header('Turismo ETV1: Concón')
-tipos = ['restaurant','hotel','museo']
+tipos = ['restaurant','hotel','museo','teatro','galeria']
 
-tipo = random.choice(tipos)
-st.write(tipo)
-
-url = f'http://quant.cl/places/Concon_{tipo}'
-df = pd.read_html(url, encoding='utf-8')[0]
-st.dataframe(df)   
-
+xdf = pd.DataFrame()
+for tipo in tipos:
+  url = f'http://quant.cl/places/Concon_{tipo}'
+  df = pd.read_html(url, encoding='utf-8')[0]
+  df['tipo'] = tipo
+  df = df.drop(columns=['open'])
+  xdf.append(df)
+  
+gdf = xdf.groupby('tipo').size()
+st.dataframe(xdf)   
+st.dataframe(gdf)
 # either map or grab map from url above!
