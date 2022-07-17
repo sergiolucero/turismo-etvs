1fec67409c63f9fbf88cca4882646dddc80fe6f8
Sergio Lucero <sergiolucero@gmail.com>
2022-07-17 14:26:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,22 @@ import time
 
 t0=time.time()
 st.header('Turismo ETV1: Concón')
-tipos = ['restaurant','hotel','museo'] #,'teatro','galeria']
 
-xdf = pd.DataFrame()
-for tipo in tipos:
-  url = f'http://quant.cl/places/Concon_{tipo}'
-  df = pd.read_html(url, encoding='utf-8')[0]
-  df['tipo'] = tipo
-  df = df.drop(columns=['opening_hours'])
-  xdf = xdf.append(df)
-  
-gdf = xdf.groupby('tipo').size()
+tipos = ['restaurant','hotel','museo'] #,'teatro','galeria']
+if False:
+    xdf = pd.DataFrame()
+    for tipo in tipos:
+      url = f'http://quant.cl/places/Concon_{tipo}'
+      df = pd.read_html(url, encoding='utf-8')[0]
+      df['tipo'] = tipo
+      df = df.drop(columns=['opening_hours'])
+      xdf = xdf.append(df)
+
+    gdf = xdf.groupby('tipo').size()
+else:
+    xdf = pd.read_csv('xdf3t.csv')
+    gdf = pd.read_pickle('gdf3t.pk')
+    
 st.write('DT:', round(time.time()-t0,2))
 st.dataframe(xdf)   
 st.dataframe(gdf)
